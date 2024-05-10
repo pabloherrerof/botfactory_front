1d0c6abeff1ff35e132c2f0b1e3954037ca8993d
Pablo Herrero Ferrán <162309731+pherrerofe@users.noreply.github.com>
2024-05-10 14:45:28 +0200

    delete not used methods

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -56,46 +56,6 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
       });
   };
 
-  const forgotPassword = async ({ setErrors, setStatus, email }) => {
-    await csrf();
-
-    setErrors([]);
-    setStatus(null);
-
-    axios
-      .post("/forgot-password", { email })
-      .then((response) => setStatus(response.data.status))
-      .catch((error) => {
-        if (error.response.status !== 422) throw error;
-
-        setErrors(error.response.data.errors);
-      });
-  };
-
-  const resetPassword = async ({ setErrors, setStatus, ...props }) => {
-    await csrf();
-
-    setErrors([]);
-    setStatus(null);
-
-    axios
-      .post("/reset-password", { token: params.token, ...props })
-      .then((response) =>
-        router.push("/login?reset=" + btoa(response.data.status))
-      )
-      .catch((error) => {
-        if (error.response.status !== 422) throw error;
-
-        setErrors(error.response.data.errors);
-      });
-  };
-
-  const resendEmailVerification = ({ setStatus }) => {
-    axios
-      .post("/email/verification-notification")
-      .then((response) => setStatus(response.data.status));
-  };
-
   const logout = async () => {
     if (!error) {
       await axios
@@ -123,8 +83,6 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
         router.push(redirectIfAuthenticated);
       } 
     }
-    if (window.location.pathname === "/verify-email" && user?.email_verified_at)
-      router.push(redirectIfAuthenticated);
     if (middleware === "auth" && error) logout();
   }, [user, error]);
 
